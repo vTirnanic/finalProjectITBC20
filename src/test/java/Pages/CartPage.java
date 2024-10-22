9959b4cf00c9327205e3d6f394e5d9b39c0d2d8d
vTirnanic <vtirnanic@gmail.com>
2024-10-22 19:53:46 +0200

    Commit14

--- src/test/java/Pages/CartPage.java
+++ src/test/java/Pages/CartPage.java
@@ -5,17 +5,41 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class CartPage extends BaseTest {
 
     public CartPage() {
         PageFactory.initElements(driver,this);
     }
 
+    @FindBy(id = "shopping_cart_container")
+    public WebElement cartIcon;
+
+    @FindBy(css = "span[class='shopping_cart_badge']")
+    public WebElement cartBadge;
+
+    @FindBy(className = "cart_quantity")
+    public WebElement quantityCell;
+
+    @FindBy(id = "continue-shopping")
+    public WebElement conShopButton;
+
     @FindBy(id = "checkout")
     public WebElement checkOutButton;
 
+    @FindBy(className = "cart_item")
+    public WebElement cartItem;
+
+    @FindBy(className = "cart_item")
+    public List<WebElement> cartItems;
+
     public void clickOnCheckOutButton() {
         checkOutButton.click();
+    }
+
+    public void clickOnConShopButton() {
+        conShopButton.click();
     }
 
 }
--- src/test/java/Test/CartTest.java
+++ src/test/java/Test/CartTest.java
@@ -1,4 +1,73 @@
 package Test;
 
-public class CartTest {
+import Base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class CartTest extends BaseTest {
+
+    String invPageURL = "https://www.saucedemo.com/inventory.html";
+
+    @BeforeMethod
+    public void pageSetUp() {
+        driver.navigate().to("https://www.saucedemo.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+    }
+
+    @Test
+    public void userCanOpenCartFromInvPage() {
+        logging();
+        inventoryPage.clickOnCartIcon();
+        driver.navigate().back();
+        inventoryPage.clickOnAtcButton(1);
+        inventoryPage.clickOnCartIcon();
+        Assert.assertTrue(cartPage.cartIcon.isDisplayed());
+        Assert.assertTrue(cartPage.cartBadge.isDisplayed());
+        Assert.assertTrue(cartPage.quantityCell.isDisplayed());
+        Assert.assertTrue(cartPage.checkOutButton.isDisplayed());
+    }
+
+    @Test
+    public void userCanContinueShoppingFromCart() {
+        logging();
+        inventoryPage.clickOnCartIcon();
+        cartPage.clickOnConShopButton();
+        inventoryPage.clickOnAtcButton(2);
+        inventoryPage.clickOnCartIcon();
+        cartPage.clickOnConShopButton();
+        Assert.assertEquals(driver.getCurrentUrl(),invPageURL);
+        Assert.assertEquals(inventoryPage.cartBadge.getText(),"1");
+    }
+
+    @Test
+    public void userCanAddOneItemtoCart() {
+        logging();
+        inventoryPage.clickOnAtcButton(2);
+        inventoryPage.clickOnCartIcon();
+        Assert.assertTrue(cartPage.cartIcon.isDisplayed());
+        Assert.assertTrue(cartPage.cartBadge.isDisplayed());
+        Assert.assertEquals(cartPage.cartBadge.getText(),"1");
+        Assert.assertTrue(cartPage.cartItem.isDisplayed());
+    }
+
+    @Test
+    public void userCanAddMultipleItemstoCart() {
+        logging();
+        inventoryPage.clickOnAtcButton(2);
+        inventoryPage.clickOnAtcButton(1);
+        scrollToElement(inventoryPage.itemTitles.get(4));
+        inventoryPage.clickOnAtcButton(3);
+        inventoryPage.clickOnCartIcon();
+        Assert.assertTrue(cartPage.cartIcon.isDisplayed());
+        Assert.assertTrue(cartPage.cartBadge.isDisplayed());
+        Assert.assertEquals(cartPage.cartBadge.getText(),"3");
+        Assert.assertEquals(cartPage.cartItems.size(),3);
+    }
+
+
+
+
 }
