bc0186ba140a26570e78d28d5c62db522db713ec
vTirnanic <vtirnanic@gmail.com>
2024-10-23 12:24:11 +0200

    Commit18

--- src/test/java/Test/LoginTest.java
+++ src/test/java/Test/LoginTest.java
@@ -1,7 +1,9 @@
 package Test;
 
 import Base.BaseTest;
+import Pages.*;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -15,8 +17,18 @@ public class LoginTest extends BaseTest {
 
     @BeforeMethod
     public void pageSetUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.navigate().to("https://www.saucedemo.com/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        homepagePage = new HomepagePage();
+        inventoryPage = new InventoryPage();
+        productPage = new ProductPage();
+        cartPage = new CartPage();
+        checkout1Page = new Checkout1Page();
+        checkout2Page = new Checkout2Page();
+        checkout3Page = new Checkout3Page();
     }
 
     @Test
--- src/test/java/Test/SortingTest.java
+++ src/test/java/Test/SortingTest.java
@@ -1,6 +1,8 @@
 package Test;
 
 import Base.BaseTest;
+import Pages.*;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -11,8 +13,18 @@ public class SortingTest extends BaseTest {
 
     @BeforeMethod
     public void pageSetUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.navigate().to("https://www.saucedemo.com/");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        homepagePage = new HomepagePage();
+        inventoryPage = new InventoryPage();
+        productPage = new ProductPage();
+        cartPage = new CartPage();
+        checkout1Page = new Checkout1Page();
+        checkout2Page = new Checkout2Page();
+        checkout3Page = new Checkout3Page();
     }
 
     @Test
@@ -53,9 +65,6 @@ public class SortingTest extends BaseTest {
         inventoryPage.selectSortOption("Price (low to high)");
         inventoryPage.itemPricesAfterSorting();
         System.out.println(inventoryPage.itemPricesAfterSorting());
-
-
-
     }
 
     @Test
