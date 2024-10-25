3621c637cfe589cf03abcdf17a15585e391633bb
vTirnanic <vtirnanic@gmail.com>
2024-10-25 11:10:22 +0200

    Commit26

--- src/test/java/Base/BaseTest.java
+++ src/test/java/Base/BaseTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
